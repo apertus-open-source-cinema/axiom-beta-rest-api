c1efbda1e1fe9781b2016c2d73ead49b10e8373d
Anudit Verma <verma.anudit@gmail.com>
2017-06-17 01:57:40 +0530

    Update client.go

--- GoAPI/client.go
+++ GoAPI/client.go
@@ -1,41 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "Schema/AxiomDaemon"
-    flatbuffers "github.com/google/flatbuffers/go"
-    "log"
+	"Schema/AxiomDaemon"
+	"fmt"
+	flatbuffers "github.com/google/flatbuffers/go"
+	"io"
+	"log"
+	"net"
+	"time"
 )
 
-type Client struct{
+func reader(r io.Reader) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := r.Read(buf[:])
+		if err != nil {
+			return
+		}
+		println("Client got:", string(buf[0:n]))
+	}
+}
 
-    func reader(r io.Reader) {
-        buf := make([]byte, 1024)
-        for {
-            n, err := r.Read(buf[:])
-            if err != nil {
-                return
-            }
-            println("Client got:", string(buf[0:n]))
-        }
-    }
+func main() {
+	c, err := net.Dial("unixgram", "/tmp/axiom_daemon")
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
 
-    func main() {
-        c, err := net.Dial("unixgram", "/tmp/axiom_daemon")
-        if err != nil {
-            panic(err)
-        }
-        defer c.Close()
-
-        go reader(c)
-        for {
-            _, err := c.Write([]byte("hi"))
-            if err != nil {
-                log.Fatal("write error:", err)
-                break
-            }
-            time.Sleep(1e9)
-        }
-    }
+	go reader(c)
+	for {
+		_, err := c.Write([]byte("hi"))
+		if err != nil {
+			log.Fatal("write error:", err)
+			break
+		}
+		time.Sleep(1e9)
+	}
 }
