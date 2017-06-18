e8de7d1d1723ecf274b585df4df5d1ec6d386b89
Anudit Verma <verma.anudit@gmail.com>
2017-06-18 20:10:39 +0530

    commented unused imports

--- GoAPI/client.go
+++ GoAPI/client.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"Schema/AxiomDaemon"
-	"fmt"
-	flatbuffers "github.com/google/flatbuffers/go"
+	//"Schema/AxiomDaemon"
+	//"fmt"
+	//flatbuffers "github.com/google/flatbuffers/go"
 	"io"
 	"log"
 	"net"
