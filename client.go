98ecc18fb914819dd0b434ee37d1b24b03d9651a
Anudit Verma <verma.anudit@gmail.com>
2017-06-29 13:01:38 +0530

    Update client.go

--- client.go
+++ client.go
@@ -17,42 +17,61 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package main
-
+ 
 import (
-	//"Schema/AxiomDaemon"
-	//"fmt"
-	//flatbuffers "github.com/google/flatbuffers/go"
-	"io"
-	"log"
-	"net"
-	"time"
+    schema "Schema/AxiomDaemon"
+    flatbuffers "github.com/google/flatbuffers/go"
+    "log"
+    "net"
 )
-
-func reader(r io.Reader) {
-	buf := make([]byte, 1024)
-	for {
-		n, err := r.Read(buf[:])
-		if err != nil {
-			return
-		}
-		println("Client got:", string(buf[0:n]))
-	}
+ 
+type Client struct {
+    socketPath string
+    Builder    *flatbuffers.Builder
+    Settings   []flatbuffers.UOffsetT
+    Socket     net.Conn
 }
-
+ 
+func (c *Client) AddSettingIS(mode schema.Mode, imageSensorSetting schema.ImageSensorSettings, parameter uint16) {
+ 
+    schema.ImageSensorSettingStart(c.Builder)
+    schema.ImageSensorSettingAddMode(c.Builder, schema.ModeWrite)
+    schema.ImageSensorSettingAddSetting(c.Builder, schema.ImageSensorSettingsGain)
+    schema.ImageSensorSettingAddParameter(c.Builder, 0)
+    is := schema.ImageSensorSettingEnd(c.Builder)
+ 
+    schema.PayloadStart(c.Builder)
+    schema.PayloadAddPayloadType(c.Builder, schema.SettingImageSensorSetting)
+    schema.PayloadAddPayload(c.Builder, is)
+    payload := schema.PayloadEnd(c.Builder)
+    c.Settings = append(c.Settings, payload)
+}
+ 
+func (c *Client) Init() {
+    c.Builder = flatbuffers.NewBuilder(1024)
+    c.Settings = make([]flatbuffers.UOffsetT, 0)
+}
+ 
 func main() {
-	c, err := net.Dial("unixgram", "/tmp/axiom_daemon")
-	if err != nil {
-		panic(err)
-	}
-	defer c.Close()
-
-	go reader(c)
-	for {
-		_, err := c.Write([]byte("hi"))
-		if err != nil {
-			log.Fatal("write error:", err)
-			break
-		}
-		time.Sleep(1e9)
-	}
+    c := new(Client)
+    c.Init()
+    c.AddSettingIS(schema.ModeWrite, schema.ImageSensorSettingsGain, 2)
+    schema.PacketStartSettingsVector(c.Builder, 1)
+    c.Builder.PrependUOffsetT(c.Settings[0])
+    settings := c.Builder.EndVector(1)
+ 
+    schema.PacketStart(c.Builder)
+    schema.PacketAddSettings(c.Builder, settings)
+    p := schema.PacketEnd(c.Builder)
+    c.Builder.Finish(p)
+ 
+    c.Builder.Finish(p)
+    bytes := c.Builder.FinishedBytes()
+ 
+    var socketPath string = "/tmp/axiom_daemon"
+    conn, err := net.Dial("unixgram", socketPath)
+    _, err = conn.Write([]byte(bytes))
+    if err != nil {
+        log.Fatal("write error:", err)
+    }
 }
